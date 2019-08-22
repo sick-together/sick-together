d787ca7dd0ef243bfa51b92fff1d57cb80b803d7
Morgan-Mischo <goramorgan@gmail.com>
2019-08-22 10:54:23 -0600

    working through things

--- src/Components/Group/Group.js
+++ src/Components/Group/Group.js
@@ -1,318 +1,210 @@
-import React, { useEffect, useState } from "react";
-import {
-  getSelectedGroup,
-  getGroupMessages,
-  addMessage,
-} from "../../Redux/groupReducer.js";
-import { connect } from "react-redux";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Avatar from "@material-ui/core/Avatar";
-import Chip from "@material-ui/core/Chip";
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-// const io = require('socket.io-client'); 
-// const socket = io('http://localhost:3000'); 
-
-import useSocket from 'use-socket.io-client'; 
-import { useImmer } from 'use-immer'; 
-
+import React, { useEffect, useState } from 'react'
+import { getSelectedGroup, getGroupMessages, getRooms, addMessage, createGeneral } from '../../Redux/groupReducer.js'
+import { connect } from 'react-redux';
+import './Group.css'
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import io from 'socket.io-client';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3, 2),
-    margin: "20px",
-    // height: '50vh'
-  },
-  flex: {
-    display: "flex",
-    alignItems: "center",
-  },
-  groupTitle: {
-    display: "flex",
-    alignItems: "center",
-  },
-  bigAvatar: {
-    margin: 10,
-    width: 60,
-    height: 60,
-  },
-  topicsWindow: {
-    width: "30%",
-    height: "300px",
-    borderRight: "1px solid grey",
-  },
-  chatWindow: {
-    width: "70%",
-    height: "300px",
-    padding: "20px",
-  },
-  chatBox: {
-    width: "85%",
-  },
-  button: {
-    width: "15%",
-  },
-  message: {
-    display: "flex",
-    alignItems: "center",
-    paddingTop: "5px",
-  },
-  chip: {
-    marginRight: "5px",
-  },
+    root: {
+        padding: theme.spacing(0, 0, 3, 0),
+        margin: '20px',
+        // height: '50vh'
+    },
+    flex: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    groupTitle: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    currentRoom: {
+        marginLeft: '15px'
+    },
+    bigAvatar: {
+        margin: 10,
+        width: 55,
+        height: 55,
+    },
+    topicsWindow: {
+        width: '20%',
+        height: '500px',
+        borderRight: '1px solid grey'
+    },
+    fab: {
+        marginLeft: '20px',
+        marginTop: '7px'
+    },
+    chatWindow: {
+        width: '100%',
+        height: '450px',
+        // padding: '20px',
+        overflowY: 'auto'
+    },
+    chatBox: {
+        width: '100%'
+    },
+    button: {
+        width: '10%'
+    },
+    textField: {
+        width: '90%',
+        display: 'flex',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0,
+        marginLeft: '25px'
+    },
+    message: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '15px 10px',
+        borderTop: '1px solid grey',
+        borderRadius: '5px'
+    },
+    messageContent: {
+        marginLeft: '5px'
+    },
+    chip: {
+        marginRight: '5px',
+        backgroundColor: '#4d92e0',
+        color: '#fff'
+    }
 }));
 
 
+
 function Group(props) {
+    let { selectedGroup, groupMessages, rooms } = props.groups
+    const classes = useStyles()
+    console.log('selected group:', selectedGroup);
+    const [textValue, changeTextValue] = React.useState('')
+    const [currentRoom, changeCurrentRoom] = React.useState('general')
 
-  let { selectedGroup, groupMessages } = props.groups;
-  const classes = useStyles();
+    useEffect(() => {
+        if (selectedGroup && selectedGroup[0]) {
+            let { group_id } = selectedGroup[0]
+            props.getGroupMessages(+group_id)
+            props.getRooms(+group_id)
+        }
+    }, [selectedGroup[0]])
 
-  const [id, setId] = useState(''); 
-  const [nameInput, setNameInput] = useState(''); 
-  const [room, setRoom] = useState(''); 
-  const [input, setInput] = useState(''); 
+    function insertMessage(newMessage, groupId, currentRoom) {
+        rooms.map(room => {
+            if (room.room_name === currentRoom) {
+                let roomId = +room.room_id
+                props.addMessage(newMessage, groupId, roomId)
+                changeTextValue('')
+            }
+        })
 
-
-  const [socket] = useSocket('https://localhost:3000'); 
-  socket.connect(); 
-
-  const[messages, setMessages] = useImmer([]); 
-  const [online, setOnline] = useImmer([]); 
-
-  useEffect(() => {
-    socket.on('messge que', (nick,message) => {
-      setMessages(draft => {
-        draft.push(nick,message)
-      })
-    })
-
-    socket.on('update', message => setMessages(draft => {
-      draft.push(['',message]); 
-    })); 
-
-    
-  })
+    }
 
 
-  // let { selectedGroup, groupMessages } = props.groups;
-  // const classes = useStyles();
-  // console.log("selected group:", selectedGroup);
-  // const [textValue, changeTextValue] = React.useState("");
-  // const [inRoom, setInRoom] = useState(false); 
+    if (selectedGroup && selectedGroup[0]) {
+        let { group_name, group_picture, description, group_id } = selectedGroup[0]
+        return (
 
-  // useEffect(() => {
-  //   if (selectedGroup && selectedGroup[0]) {
-  //     let { group_id } = selectedGroup[0];
-  //     props.getGroupMessages(+group_id);
-  //   }
-  // }, [selectedGroup[0]]);
+            <div className='Group'>
+                <Paper className={classes.root}>
+                    <Paper>
+                        <section className={classes.flex}>
+                            <Avatar alt="Group Avatar" src={group_picture} className={classes.bigAvatar} />
+                            <div className={classes.groupTitle}>
+                                <Typography variant='h5' component='h5'>
+                                    {group_name}
+                                </Typography>
+                                <Typography component='p'>
+                                    #{currentRoom}
+                                </Typography>
+                            </div>
+                        </section>
 
-  // useEffect(() => {
-  //   if(inRoom) {
-  //     console.log('joining room'); 
-  //     socket.emit('room', {room: 'test-room'}); 
-  //   }
-  //   return () => {
-  //     if(inRoom) {
-  //       console.log('leaving room'); 
-  //       socket.emit('leave room', {
-  //         room: 'test-room'
-  //       })
-  //     }
-  //   }
-  // }); 
+                    </Paper>
 
-  // const handleInRoom = () => {
-  //   inRoom 
-  //   ? setInRoom(false)
-  //   : setInRoom(true); 
-  // }
+                    <section className={classes.flex}>
+                        <div className={classes.topicsWindow}>
+                            <List>
+                                {
+                                    rooms.map(topic => {
+                                            return (
+                                                <ListItem button key={topic.room_id} onClick={() => changeCurrentRoom(topic.room_name)}>
+                                                    <ListItemText>#{topic.room_name}</ListItemText>
+                                                </ListItem>
+                                            )
+                                        
+                                    })
+                                }
+                                <Fab color="primary" aria-label="add" size='small' className={classes.fab}>
+                                    <AddIcon />
+                                </Fab>
+                            </List>
+                        </div>
+                        <div style={{ width: '100%', position: 'relative', height: '500px' }}>
+                            <section className={classes.chatWindow}>
+                                {groupMessages && groupMessages.length ?
+                                    groupMessages.map((message, index) => {
+                                        if (message.room_name === currentRoom) {
+                                            return (
+                                                <div className={classes.message} key={index}>
+                                                    <Chip label={message.username} avatar={<Avatar alt={message.username} src={message.profile_pic} />} className={classes.chip} />
+                                                    <Typography varient='p' className={classes.messageContent}>
+                                                        {message.message}
+                                                    </Typography>
+                                                </div>
+                                            )
+                                        } else return null
+                                    }) : <Typography varient='p'>
+                                        This chat is empty!
+                                    </Typography>
+                                }
 
-  // const handleNewMessage = () => {
-  //   console.log('emitting new message'); 
-  //   socket.emit('new message', {
-  //     room: 'test-room'
-  //   }); 
-  // }
+                            </section>
+                            <section className={classes.textField}>
+                                <TextField
+                                    id="standard-name"
+                                    label="Send a message!"
+                                    className={classes.chatBox}
+                                    value={textValue}
+                                    onChange={e => changeTextValue(e.target.value)}
+                                    margin="normal"
+                                />
+                                <Button variant="contained" color="primary" className={classes.button} onClick={() => insertMessage(textValue, group_id, currentRoom)}>
+                                    Send
+                            <Icon className={classes.rightIcon}>send</Icon>
+                                </Button>
+                            </section>
+                        </div>
 
-
-  // function insertMessage(newMessage, groupId) {
-  //   props.addMessage(newMessage, groupId);
-  //   changeTextValue("");
-  // }
-
-  // if (selectedGroup && selectedGroup[0]) {
-  //   let { group_name, group_picture, description, group_id } = selectedGroup[0];
-    return (
-      
-      <div>
-        <Paper className={classes.root}>
-          <Typography
-            variant="h5"
-            component="h3"
-            className={classes.groupTitle}
-          >
-            <Avatar
-              alt="Group Avatar"
-              src={group_picture}
-              className={classes.bigAvatar}
-            />
-            {group_name}
-          </Typography>
-
-          <section className={classes.flex}>
-            <div className={classes.topicsWindow}>
-              <List>
-                {["#general", "#stories", "#random", "#memes"].map(topic => (
-                  <ListItem button>
-                    <ListItemText primary={topic} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-            <div className={classes.chatWindow}>
-              {groupMessages && groupMessages.length ? (
-                groupMessages.map((message, index) => (
-                  <div className={classes.message} key={index}>
-                    <Chip label={message.username} className={classes.chip} />
-                    <Typography varient="p">{message.message}</Typography>
-                  </div>
-                ))
-              ) : (
-                <Typography varient="p">This chat is empty!</Typography>
-              )}
-            </div>
-          </section>
-          <section className={classes.flex}>
-            <TextField
-              id="standard-name"
-              label="Send a message!"
-              className={classes.chatBox}
-              value={textValue}
-              onChange={e => changeTextValue(e.target.value)}
-              margin="normal"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              onClick={() => insertMessage(textValue, group_id)}
-            >
-              Send
-              <Icon className={classes.rightIcon}>send</Icon>
-            </Button>
-          </section>
-        </Paper>
-      </div>
-    );
-  } else return null;
+                    </section>
+                </Paper>
+            </div >
+        )
+    } else return null
 }
 
 function mapStateToProps(state) {
-  return {
-    user: state.user,
-    groups: state.groups,
-  };
+    return {
+        user: state.user,
+        groups: state.groups
+    }
 }
 export default connect(
-  mapStateToProps,
-  { getSelectedGroup, getGroupMessages, addMessage }
+    mapStateToProps,
+    { getSelectedGroup, getGroupMessages, getRooms, addMessage, createGeneral }
 )(Group);
-
-// class Group extends Component {
-//     constructor() {
-//       super();
-//       this.state = {
-//         input: '',
-//         messages: [],
-//         room: '',
-//         joined: false
-//       }
-//       this.joinRoom = this.joinRoom.bind(this);
-//       this.joinSuccess = this.joinSuccess.bind(this);
-//       this.sendMessage = this.sendMessage.bind(this);
-//       this.updateMessages = this.updateMessages.bind(this);
-//     }
-//     componentDidMount() {
-//       this.socket = io();
-//       this.socket.on('room joined', data => {
-//         this.joinSuccess(data)
-//       })
-//       this.socket.on('message dispatched', data => {
-//         console.log(data)
-//         this.updateMessages(data);
-//       })
-//     }
-//     componentWillUnmount() {
-//       this.socket.disconnect();
-//     }
-//     sendMessage() {
-//       this.socket.emit('message sent', {
-//         message: this.state.input,
-//         room: this.state.room
-//       })
-//       this.setState({
-//         input: ''
-//       })
-//     }
-//     updateMessages(messages) {
-//       this.setState({
-//         messages
-//       })
-//     }
-//     joinRoom() {
-//       if (this.state.room) {
-//         this.socket.emit('join room', {
-//           room: this.state.room
-//         })
-//       }
-//     }
-//     joinSuccess(messages) {
-//       this.setState({
-//         joined: true,
-//         messages
-//       })
-//     }
-//     render() {
-//       console.log(this.state.messages)
-//       return (
-//         <div className="App">
-//           {this.state.joined ? <h1>My Room: {this.state.room}</h1> : null}
-//           <div>
-//             {this.state.messages.map(messageObj => <h2 key={messageObj.id}>{messageObj.message}</h2>)}
-//           </div>
-//           {
-//             this.state.joined
-//               ?
-//               <div>
-//                 <input value={this.state.input} onChange={e => {
-//                   this.setState({
-//                     input: e.target.value
-//                   })
-//                 }} />
-//                 <button onClick={this.sendMessage}>Send</button>
-//               </div>
-//               :
-//               <div>
-//                 <input value={this.state.room} onChange={e => {
-//                   this.setState({
-//                     room: e.target.value
-//                   })
-//                 }} />
-//                 <button onClick={this.joinRoom}>Join</button>
-//               </div>
-//           }
-//         </div>
-//       );
-//     }
-//   }
-  
-  // export default Group; 

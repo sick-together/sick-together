66aff78f1b5fd33e850522ae66afaffb0e5b8382
Morgan-Mischo <goramorgan@gmail.com>
2019-09-01 19:30:18 -0600

    last of my jest tests

--- src/Components/_tests_/morgans.test.js
+++ src/Components/_tests_/morgans.test.js
@@ -1,8 +1,38 @@
 import {
     handleClose, 
-    open
+    handleOpen, 
+    handleChange, 
+    handleSubmit, 
+    fileUploadHandler
 } from "../logic/logic"; 
+import { exportAllDeclaration } from "@babel/types";
 
 describe("Tests handleClose function", () => {
-    it("should be a function", )
-})
+    it("should be a function", () => {
+        expect(typeof handleClose).toBe('function')
+    })
+}); 
+
+describe("Tests handleOpen function", () => {
+    it("should be a function", () => {
+        expect(typeof handleOpen).toBe('function')
+    })
+}); 
+
+describe("Tests handleChange function", () => {
+    it("should be a function", () => {
+        expect(typeof handleChange).toBe('function')
+    })
+}); 
+
+describe("Tests handleSubmit function", () => {
+    it("should be a function", () => {
+        expect(typeof handleSubmit).toBe('function')
+    })
+}); 
+
+describe("Tests fileUploadHandler function", () => {
+    it("should be a function", () => {
+        expect(typeof fileUploadHandler).toBe('function')
+    })
+}); 
--- src/Components/logic/logic.js
+++ src/Components/logic/logic.js
@@ -1,8 +1,36 @@
 import axios from 'axios'; 
 import React, { useRef } from "react";
+import firebase from 'firebase'; 
 
 const [open, setOpen] = React.useState(false);
+const [state, setStateUS] = React.useState("");
+const [group_name, setGroupName] = React.useState("");
+const [group_picture, setGroupPicture] = React.useState("");
+const [description, setDescription] = React.useState("");
+const [city, setCity] = React.useState("");
 
 export function handleClose() {
     setOpen(false); 
 }
+
+export function handleOpen() {
+    setOpen(true);
+  }
+
+  export function handleChange(e) {
+    setStateUS(e.target.value);
+  }
+
+  export function handleSubmit() {
+    setGroupName("");
+    setGroupPicture("");
+    setDescription("");
+    let location = `${city}, ${state}`
+    setCity("")
+    setStateUS("")
+  }
+
+  export function fileUploadHandler(filename) {
+    firebase.storage().ref('uploads').child(filename).getDownloadURL()
+      .then(url => setGroupPicture(url))
+  }
